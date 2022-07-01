861f91c614aff83c4fc87ced10ba4fd567bcb1c5
Jacob Jackson <jacobjackson1985@icloud.com>
2022-07-01 16:49:23 -0500

    Initialize repository

---
+++ lib/actionjdjackson_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "actionjdjackson_palindrome/version"
+
+module ActionjdjacksonPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
